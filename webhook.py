0533dc3abbc7c56630b895b11ea2cf6341d03984
zul0996 <mjefferson006@gmail.com>
2020-07-21 22:19:14 -0400

    Add files via upload

--- webhook.py
+++ webhook.py
@@ -9,7 +9,7 @@ from flask import make_response
 # Flask app should start in global layout
 app = Flask(__name__)
 
-@app.route('/webhook', methods=['POST'])
+@app.route('/webhook', methods=['GET'])
 def webhook():
     req = request.get_json(silent=True, force=True)
     print(json.dumps(req, indent=4))
